add420a93d97f3c731a5f8254d529ee3e63340d2
SHINSUNGJIN <pxp13716@gmail.com>
2023-10-06 16:47:38 +0900

    .

--- JavaScript/Chap06_Class/A01Class.js
+++ JavaScript/Chap06_Class/A01Class.js
@@ -1,5 +1,4 @@
-console.log('---------------- [697] Function -----------------');
-
+console.log('---------------- Function -----------------');
 const Jumsu = (function () {
   function Jumsu(name, kor, eng) {
     this.name = name;
@@ -26,3 +25,81 @@ console.log(nolbu);
 console.log('');
 
 console.log('---------------- Class -----------------');
+// ES2015. 생성자 함수를 대체 => class
+class JumsuTwo {
+  constructor(name, kor, eng) {
+    // const this = {};
+    this._name = name;
+    this.kor = kor;
+    this.eng = eng;
+
+    this.getName = function () {
+      return this._name
+    }
+    // return this;
+  }
+
+  // constructor 바깥 영역은 class JumsuTwo의 prototype 영역이다
+  onTotal() {
+    return this.kor + this.eng;
+  };
+  onAvg() {
+    return this.onTotal() / 2;
+  };
+  display() {
+    console.log(`${this._name} / ${this.onTotal()} / ${this.onAvg()}`);
+  };
+
+  // static
+  static JumsuTwoName = '점수 구하는 프로그램'
+  static getArea(x, y) {
+    return x * y;
+  }
+
+  // 접근자 프로퍼티
+  // 구현은 함수형태로 구현하지만 외부에서 사용은 프로퍼티(속성) 형태로 한다...
+  get name() {
+    return this._name;
+  }
+  // set name(name) {
+  //   // Guard
+  //   this._name = name;
+  // }
+}
+const hungbu = new JumsuTwo('흥부', 100, 80);  // class의 constructor()가 호출된다
+const bangja = new JumsuTwo('방자', 90, 80);
+
+console.log(hungbu);
+console.log(bangja);
+
+hungbu.display();
+bangja.display();
+console.log('');
+
+// static property, static method 참조
+console.log(JumsuTwo.JumsuTwoName);
+console.log(JumsuTwo.getArea(10, 20));
+console.log('');
+
+// 접근자 프로퍼티 - 생성자 함수에서는 구현할 수 없었음
+// 객체에서는 가능 했음 - Chap03_Object의 A03ProtoType.js 참조
+console.log(hungbu.name);
+
+hungbu.name = '변경된 흥부';
+console.log(hungbu.name);
+console.log('');
+
+console.log('---------------- prototype -----------------');
+// hungbu 인스턴스를 생성한 객체는 class JumsuTwo
+console.log(hungbu.__proto__);      // JumsuTwo의 prototype
+console.log(Object.getPrototypeOf(hungbu) === JumsuTwo.prototype);
+console.log(Object.getPrototypeOf(hungbu).constructor);
+
+// hungbu 인스턴스 부모에 JumsuTwo 가 존재하니?
+console.log(hungbu instanceof JumsuTwo);    // true
+console.log(hungbu instanceof Object);      // true
+console.log('');
+
+console.log('name' in hungbu);        // true
+console.log('_name' in hungbu);       // true
+console.log('address' in hungbu);     // false
--- JavaScript/Chap06_Class/A02ESNext_Class.js
+++ JavaScript/Chap06_Class/A02ESNext_Class.js
@@ -1,6 +1,9 @@
+// ES2022에 private 속성이 추가됨
+// private => this(메모리에 만들어진 영역 - 인스턴스) 요소에서만 서로 참조가 가능한 변수
 class Jumsu {
+  #name;
   constructor(name, kor, eng) {
-    this._name = name;
+    this.#name = name;
     this._kor = kor;
     this._eng = eng;
   }
@@ -12,16 +15,25 @@ class Jumsu {
     return this.onTotal() / num;
   }
   display() {
-    console.log(`${this._name}님의 총점은 ${this.onTotal()}이고 평균은 ${this.onAvg(2)}입니다`);
+    console.log(`${this.#name}님의 총점은 ${this.onTotal()}이고 평균은 ${this.onAvg(2)}입니다`);
   }
 
   get name() {
-    return this._name;
+    return this.#name;
   }
   set name(name) {
-    if (typeof name === 'string') this._name = name;
+    if (typeof name === 'string') this.#name = name;
   }
 }
 
 const nolbu = new Jumsu('NolBu', 100, 90);
 console.log(nolbu);
+
+// console.log(nolbu.#name);
+// nolbu.#name = 'ABC';
+
+// 접근자 프로퍼티를 이용해서 private 속성에 접근할 수 밖에 없다
+nolbu.name = 'ABC';
+console.log(nolbu);
+
+console.log(nolbu.name);
