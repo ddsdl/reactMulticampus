df0a97d6574cb8047e7db57833af6e3fc65cab1b
SHINSUNGJIN <pxp13716@gmail.com>
2023-11-17 15:02:16 +0900

    .

--- Node/Chap03_NodeModule/A08Buffer.js
+++ Node/Chap03_NodeModule/A08Buffer.js
@@ -5,3 +5,12 @@ Node.js 공식문서에서는 Buffer를 다음과 같이 정의합니다.
 이 Buffer클래스는 Node.js의 일부로 도입되어 TCP 스트림이나 파일시스템같은 작업에서의 octet 스트림과의 상호작용을 가능하기 위해 만들어졌습니다.
 * octet Stream은 일반적으로 8bit 형식으로 된 데이터를 의미합니다.
 */
+// 생성
+const buffer = Buffer.from('Hello World');
+console.log(buffer);
+console.log(buffer.toString());   // buffer date를 문자로
+console.log(buffer.length);
+console.log('');
+
+// Buffer 결합
+const 
