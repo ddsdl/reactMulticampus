2a8a3fc890d03daba798574af2ea9c8add18afac
SHINSUNGJIN <pxp13716@gmail.com>
2023-11-17 14:33:33 +0900

    .

--- Node/Chap03_NodeModule/A06Event.js
+++ Node/Chap03_NodeModule/A06Event.js
@@ -7,7 +7,8 @@ Event
   Node는 http라는 module을 이용하여 Web Server를 실행하며, 이 module은 Observer pattern을 이용한다.
   Node에서는 이 Observer pattern을 EventEmitter를 이용하여 실현한다.
 
-  EventEmitter 자체는 Observer pattern의 Subject(감시 대상)로 기능한다. EventEmitter에 대한 Observer는 일반적으로 Listener라 한다.
+  EventEmitter 자체는 Observer pattern의 Subject(감시 대상)로 기능한다. 
+  EventEmitter에 대한 Observer는 일반적으로 Listener라 한다.
   EventEmitter Instance의 on()이라는 메서드에 Event Name과 callback을 전달하여 Listener를 등록한다. (리스너가 받은 인수는 이벤트에 따라 다르다)
 
   EventEmitter의 주요 instance method는 다음과 같다.
@@ -21,5 +22,36 @@ Event
     off(event, Listener)
       지정한 Event(첫번째 인수, 문자열)에 등록된 Listener(두번째 인수, 콜백함수)를 삭제한다.
 */
-
 console.log('--------------- EventEmiiter ----------------');
+const events = require('events');
+
+const emitter = new events.EventEmitter();
+
+// 메모리에 등록되어 있는 이벤트 리스너들...
+const startEvent = () => {
+  console.log('START');
+}
+const threeEvent = (x) => {
+  console.log('THREE 1=> ' + x);
+}
+const fiveEvent = (x) => {
+  console.log('FIVE=> ' + x);
+}
+const endEvent = () => {
+  console.log('END');
+  emitter.off('start', startEvent);
+  emitter.off('three', threeEvent);
+  emitter.off('five', fiveEvent);
+  emitter.off('end', endEvent);
+}
+
+emitter.on('start', startEvent);
+emitter.on('three', threeEvent);
+emitter.on('three', threeEvent);
+emitter.on('five', fiveEvent);
+emitter.on('end', endEvent);
+
+emitter.emit('start');
+emitter.emit('three', 3);
+emitter.emit('five', 3);
+emitter.emit('end');
--- Node/Chap03_NodeModule/A07EventMax.js
+++ Node/Chap03_NodeModule/A07EventMax.js
@@ -1,3 +1,16 @@
 const events = require('events');
 
 const emitter = new events.EventEmitter();
+
+// listener는 동일한 이름으로 10까지만 등록이 가능하다.
+// 생성한 Event 객체별로 최대 수량 등록
+// emitter.setMaxListeners(15);
+
+// 이벤트 객체 자체에 수량을 변경. 이벤트 객체는 이 기본값을 따른다
+events.EventEmitter.defaultMaxListeners = 15;
+
+for (let i = 0; i < 11; i++) {
+  emitter.on('start', () => console.log(`START: ${i}`));
+}
+emitter.emit('start');
+console.log('')
