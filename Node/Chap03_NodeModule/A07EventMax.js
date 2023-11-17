68ed76014c3dbe3bd1720837f776d2024dca21ab
SHINSUNGJIN <pxp13716@gmail.com>
2023-11-17 14:44:04 +0900

    .

--- Node/Chap03_NodeModule/A07EventMax.js
+++ Node/Chap03_NodeModule/A07EventMax.js
@@ -13,4 +13,24 @@ for (let i = 0; i < 11; i++) {
   emitter.on('start', () => console.log(`START: ${i}`));
 }
 emitter.emit('start');
-console.log('')
+console.log('');
+
+// Event listener가 전역 객체. 따러서 변수 SCope와는 상관없다
+{
+  const msgEvent = () => console.log('Message Event');
+  emitter.on('message', msgEvent);
+
+  emitter.emit('message');
+
+  // scope와 상관없으르모 사용하지 않을거면 삭제해야 한다
+  emitter.off('message', msgEvent);
+}
+
+emitter.emit('message');
+console.log('');
+
+// Event 자체가 Error 라면
+// Event에는 콜백 패턴과 같이 error라는 이름으로 이미 정의되어 있다
+emitter.on('error', (err) => console.error(err.message));
+
+emitter.emit('error', new Error('전달될 에러 메시지'));
