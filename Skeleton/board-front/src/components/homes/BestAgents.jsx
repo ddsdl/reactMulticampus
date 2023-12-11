c8fbe5edae2e9b86d993af3b1594e1c1277ed372
SHINSUNGJIN <pxp13716@gmail.com>
2023-12-11 13:07:40 +0900

    .

---
+++ Skeleton/board-front/src/components/homes/BestAgents.jsx
@@ -0,0 +1,192 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function BestAgents() {
+  return (
+    <section className="section-agents section-t8">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="title-wrap d-flex justify-content-between">
+              <div className="title-box">
+                <h2 className="title-a">Best Agents</h2>
+              </div>
+              <div className="title-link">
+                <a href="agents-grid.html"
+                >All Agents
+                  <span className="bi bi-chevron-right"></span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card-box-d">
+              <div className="card-img-d">
+                <img src="images/agent-4.jpg" alt="" className="img-d img-fluid" />
+              </div>
+              <div className="card-overlay card-overlay-hover">
+                <div className="card-header-d">
+                  <div className="card-title-d align-self-center">
+                    <h3 className="title-d">
+                      <a href="agent-single.html" className="link-two"
+                      >Margaret Sotillo <br />
+                        Escala</a
+                      >
+                    </h3>
+                  </div>
+                </div>
+                <div className="card-body-d">
+                  <p className="content-d color-text-a">
+                    Sed porttitor lectus nibh, Cras ultricies ligula sed magna dictum porta two.
+                  </p>
+                  <div className="info-agents color-a">
+                    <p><strong>Phone: </strong> +54 356 945234</p>
+                    <p><strong>Email: </strong> agents@example.com</p>
+                  </div>
+                </div>
+                <div className="card-footer-d">
+                  <div className="socials-footer d-flex justify-content-center">
+                    <ul className="list-inline">
+                      <li className="list-inline-item">
+                        <Link href="#" className="link-one">
+                          <i className="bi bi-facebook" aria-hidden="true"></i>
+                        </Link>
+                      </li>
+                      <li className="list-inline-item">
+                        <Link href="#" className="link-one">
+                          <i className="bi bi-twitter" aria-hidden="true"></i>
+                        </Link>
+                      </li>
+                      <li className="list-inline-item">
+                        <Link href="#" className="link-one">
+                          <i className="bi bi-instagram" aria-hidden="true"></i>
+                        </Link>
+                      </li>
+                      <li className="list-inline-item">
+                        <Link href="#" className="link-one">
+                          <i className="bi bi-linkedin" aria-hidden="true"></i>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card-box-d">
+              <div className="card-img-d">
+                <img src="images/agent-1.jpg" alt="" className="img-d img-fluid" />
+              </div>
+              <div className="card-overlay card-overlay-hover">
+                <div className="card-header-d">
+                  <div className="card-title-d align-self-center">
+                    <h3 className="title-d">
+                      <a href="agent-single.html" className="link-two"
+                      >Stiven Spilver <br />
+                        Darw</a
+                      >
+                    </h3>
+                  </div>
+                </div>
+                <div className="card-body-d">
+                  <p className="content-d color-text-a">
+                    Sed porttitor lectus nibh, Cras ultricies ligula sed magna dictum porta two.
+                  </p>
+                  <div className="info-agents color-a">
+                    <p><strong>Phone: </strong> +54 356 945234</p>
+                    <p><strong>Email: </strong> agents@example.com</p>
+                  </div>
+                </div>
+                <div className="card-footer-d">
+                  <div className="socials-footer d-flex justify-content-center">
+                    <ul className="list-inline">
+                      <li className="list-inline-item">
+                        <Link href="#" className="link-one">
+                          <i className="bi bi-facebook" aria-hidden="true"></i>
+                        </Link>
+                      </li>
+                      <li className="list-inline-item">
+                        <Link href="#" className="link-one">
+                          <i className="bi bi-twitter" aria-hidden="true"></i>
+                        </Link>
+                      </li>
+                      <li className="list-inline-item">
+                        <Link href="#" className="link-one">
+                          <i className="bi bi-instagram" aria-hidden="true"></i>
+                        </Link>
+                      </li>
+                      <li className="list-inline-item">
+                        <Link href="#" className="link-one">
+                          <i className="bi bi-linkedin" aria-hidden="true"></i>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card-box-d">
+              <div className="card-img-d">
+                <img src="images/agent-5.jpg" alt="" className="img-d img-fluid" />
+              </div>
+              <div className="card-overlay card-overlay-hover">
+                <div className="card-header-d">
+                  <div className="card-title-d align-self-center">
+                    <h3 className="title-d">
+                      <a href="agent-single.html" className="link-two"
+                      >Emma Toledo <br />
+                        Cascada</a
+                      >
+                    </h3>
+                  </div>
+                </div>
+                <div className="card-body-d">
+                  <p className="content-d color-text-a">
+                    Sed porttitor lectus nibh, Cras ultricies ligula sed magna dictum porta two.
+                  </p>
+                  <div className="info-agents color-a">
+                    <p><strong>Phone: </strong> +54 356 945234</p>
+                    <p><strong>Email: </strong> agents@example.com</p>
+                  </div>
+                </div>
+                <div className="card-footer-d">
+                  <div className="socials-footer d-flex justify-content-center">
+                    <ul className="list-inline">
+                      <li className="list-inline-item">
+                        <Link href="#" className="link-one">
+                          <i className="bi bi-facebook" aria-hidden="true"></i>
+                        </Link>
+                      </li>
+                      <li className="list-inline-item">
+                        <Link href="#" className="link-one">
+                          <i className="bi bi-twitter" aria-hidden="true"></i>
+                        </Link>
+                      </li>
+                      <li className="list-inline-item">
+                        <Link href="#" className="link-one">
+                          <i className="bi bi-instagram" aria-hidden="true"></i>
+                        </Link>
+                      </li>
+                      <li className="list-inline-item">
+                        <Link href="#" className="link-one">
+                          <i className="bi bi-linkedin" aria-hidden="true"></i>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default BestAgents;
---
+++ Skeleton/board-front/src/components/homes/LatestNews.jsx
@@ -0,0 +1,140 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function LatestNews() {
+  return (
+    <section className="section-news section-t8">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="title-wrap d-flex justify-content-between">
+              <div className="title-box">
+                <h2 className="title-a">Latest News</h2>
+              </div>
+              <div className="title-link">
+                <a href="blog-grid.html"
+                >All News
+                  <span className="bi bi-chevron-right"></span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="news-carousel" className="swiper">
+          <div className="swiper-wrapper">
+            <div className="carousel-item-c swiper-slide">
+              <div className="card-box-b card-shadow news-box">
+                <div className="img-box-b">
+                  <img src="images/post-2.jpg" alt="" className="img-b img-fluid" />
+                </div>
+                <div className="card-overlay">
+                  <div className="card-header-b">
+                    <div className="card-category-b">
+                      <Link href="#" className="category-b">House</Link>
+                    </div>
+                    <div className="card-title-b">
+                      <h2 className="title-2">
+                        <a href="blog-single.html"
+                        >House is comming <br />
+                          new</a
+                        >
+                      </h2>
+                    </div>
+                    <div className="card-date">
+                      <span className="date-b">18 Sep. 2017</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* <!-- End carousel item --> */}
+
+            <div className="carousel-item-c swiper-slide">
+              <div className="card-box-b card-shadow news-box">
+                <div className="img-box-b">
+                  <img src="images/post-5.jpg" alt="" className="img-b img-fluid" />
+                </div>
+                <div className="card-overlay">
+                  <div className="card-header-b">
+                    <div className="card-category-b">
+                      <Link href="#" className="category-b">Travel</Link>
+                    </div>
+                    <div className="card-title-b">
+                      <h2 className="title-2">
+                        <a href="blog-single.html"
+                        >Travel is comming <br />
+                          new</a
+                        >
+                      </h2>
+                    </div>
+                    <div className="card-date">
+                      <span className="date-b">18 Sep. 2017</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* <!-- End carousel item --> */}
+
+            <div className="carousel-item-c swiper-slide">
+              <div className="card-box-b card-shadow news-box">
+                <div className="img-box-b">
+                  <img src="images/post-7.jpg" alt="" className="img-b img-fluid" />
+                </div>
+                <div className="card-overlay">
+                  <div className="card-header-b">
+                    <div className="card-category-b">
+                      <Link href="#" className="category-b">Park</Link>
+                    </div>
+                    <div className="card-title-b">
+                      <h2 className="title-2">
+                        <a href="blog-single.html"
+                        >Park is comming <br />
+                          new</a
+                        >
+                      </h2>
+                    </div>
+                    <div className="card-date">
+                      <span className="date-b">18 Sep. 2017</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* <!-- End carousel item --> */}
+
+            <div className="carousel-item-c swiper-slide">
+              <div className="card-box-b card-shadow news-box">
+                <div className="img-box-b">
+                  <img src="images/post-3.jpg" alt="" className="img-b img-fluid" />
+                </div>
+                <div className="card-overlay">
+                  <div className="card-header-b">
+                    <div className="card-category-b">
+                      <Link href="#" className="category-b">Travel</Link>
+                    </div>
+                    <div className="card-title-b">
+                      <h2 className="title-2">
+                        <Link href="#">Travel is comming <br />
+                          new</Link>
+                      </h2>
+                    </div>
+                    <div className="card-date">
+                      <span className="date-b">18 Sep. 2017</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* <!-- End carousel item --> */}
+          </div>
+        </div>
+
+        <div className="news-carousel-pagination carousel-pagination"></div>
+      </div>
+    </section>
+  )
+}
+
+export default LatestNews;
